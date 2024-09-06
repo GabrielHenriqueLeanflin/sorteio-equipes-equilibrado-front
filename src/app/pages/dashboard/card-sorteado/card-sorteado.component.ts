2a0a4ccef72db03ff971ce7bf6623b089795cc7f
Wendy Souza <contato.wendy@hotmail.com>
2024-09-06 16:23:25 -0300

    feature: criando component card-sorteado.

---
+++ src/app/pages/dashboard/card-sorteado/card-sorteado.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-sorteado',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-sorteado.component.html',
+  styleUrl: './card-sorteado.component.scss'
+})
+export class CardSorteadoComponent {
+
+}
