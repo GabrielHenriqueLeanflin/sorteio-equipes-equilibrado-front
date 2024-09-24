355beaf81cceac78f14896f49c310cd245ffab10
Gabriel <gabriel.leanflin@hotmail.com>
2024-09-24 18:24:11 -0300

    feat: Pegando dados dos jogadores pelo id.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  api_host: 'https://www.api-see.com.br',
-  //api_host: 'http://localhost:8000'
+  api: 'https://www.api-see.com.br',
+  //api: 'http://localhost:8000'
 };
